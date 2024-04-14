8a91bd297dab518841fed896a2412d4d7422845f
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2024-04-14 23:00:57 +0900

    counter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final counterProvider = StateProvider((ref) => 0,);
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp(),));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends HookConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    StateController<int> counterNotifier = ref.watch(counterProvider.notifier);
+    final count = ref.watch(counterProvider);
+
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('$count'),
+              TextButton(
+                onPressed: () { 
+                  counterNotifier.state++; 
+                },
+                child: const Text('Button')),
+            ]
+          ),
         ),
       ),
     );
