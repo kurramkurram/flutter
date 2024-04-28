b903d1ade267df22d0d82fb72ad97b85f45409f1
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2024-04-28 22:23:47 +0900

    [Refactor] setting view

--- lib/ui/settings/settings_view.dart
+++ lib/ui/settings/settings_view.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_app/lang/l10n.dart';
 import 'package:flutter_app/ui/drawer/drawer_view.dart';
@@ -6,25 +5,26 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class SettingsView extends HookConsumerWidget {
   SettingsView({super.key});
-  
+
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-  
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final l10n = L10n.of(context)!;
-    
+
     return Scaffold(
-       key: _scaffoldKey,
-       appBar: AppBar(
-        leading: IconButton(icon: const Icon(Icons.menu), onPressed: () {
-          _scaffoldKey.currentState?.openDrawer();
-        },),
+      key: _scaffoldKey,
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {
+            _scaffoldKey.currentState?.openDrawer();
+          },
+        ),
         title: Text(l10n.settings),
       ),
       drawer: const DrawerView(),
-      body: Center(
-        child: Text(l10n.settings)
-      ),
+      body: Center(child: Text(l10n.settings)),
     );
   }
 }
