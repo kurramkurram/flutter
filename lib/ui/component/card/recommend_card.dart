b22e6e56bd3a73d52e02a1e8e8740622eb3be5d1
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2024-05-11 19:58:29 +0900

    [Add] cached recomend card #10

--- lib/ui/component/card/recommend_card.dart
+++ lib/ui/component/card/recommend_card.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/data/data_source/book_remote_data_source.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -13,11 +14,11 @@ class RecommendCard extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Card(
-      elevation: 10,
-      child: Image.network(
-        'https://covers.openlibrary.org/b/isbn/${docs.isbn?.first}-M.jpg',
+    return Center(
+      child: CachedNetworkImage(
         fit: BoxFit.fill,
+        imageUrl:
+            'https://covers.openlibrary.org/b/isbn/${docs.isbn?.first}-M.jpg',
       ),
     );
   }
