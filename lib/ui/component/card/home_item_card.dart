8b0ba597d3b56595428d4d240787686e93335642
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2024-05-23 23:28:03 +0900

    [Add] home grid view

---
+++ lib/ui/component/card/home_item_card.dart
@@ -0,0 +1,29 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/data/data_source/book_remote_data_source.dart';
+import 'package:flutter_app/util/log.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class HomeItemcard extends HookConsumerWidget {
+  const HomeItemcard({
+    super.key,
+    required this.docs,
+  });
+
+  /// 書籍の情報
+  final Docs docs;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Center(
+        child: InkWell(
+      onTap: () => {Log.d('title - ${docs.title}')},
+      child: CachedNetworkImage(
+        fit: BoxFit.fill,
+        width: 100,
+        imageUrl:
+            'https://covers.openlibrary.org/b/isbn/${docs.isbn?.first}-M.jpg',
+      ),
+    ));
+  }
+}
--- lib/ui/home/home_view_model.dart
+++ lib/ui/home/home_view_model.dart
@@ -10,6 +10,25 @@ final homeViewModelProvider = AsyncNotifierProvider<HomeViewModel, Books>(
 class HomeViewModel extends AsyncNotifier<Books> {
   @override
   FutureOr<Books> build() {
-    return const Books([]);
+    return Books([
+      Docs('The Toyota Way', [], ['9780071392310']),
+      Docs('One Piece', [], ['9788468471525']),
+      Docs('Atomic Kotlin', [], ['0981872557']),
+      Docs('名探偵コナン', [], ['409850717X']),
+      Docs('Steve Jobs', [], ['2709638320']),
+      Docs('Harry Potter', [], ['9788498384505']),
+      Docs('The Toyota Way', [], ['9780071392310']),
+      Docs('One Piece', [], ['9788468471525']),
+      Docs('Atomic Kotlin', [], ['0981872557']),
+      Docs('名探偵コナン', [], ['409850717X']),
+      Docs('Steve Jobs', [], ['2709638320']),
+      Docs('Harry Potter', [], ['9788498384505']),
+      Docs('The Toyota Way', [], ['9780071392310']),
+      Docs('One Piece', [], ['9788468471525']),
+      Docs('Atomic Kotlin', [], ['0981872557']),
+      Docs('名探偵コナン', [], ['409850717X']),
+      Docs('Steve Jobs', [], ['2709638320']),
+      Docs('Harry Potter', [], ['9788498384505']),
+    ]);
   }
 }
