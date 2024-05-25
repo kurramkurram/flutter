a3b871d59dff6e6ceb4118036fc7d7e2ca528e62
GM <54581188+kurramkurram@users.noreply.github.com>
2024-05-25 22:49:03 +0900

    [Add] home grid view #5 (#32)

--- lib/ui/home/home_view.dart
+++ lib/ui/home/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/data/repository/permission_repository.dart';
 import 'package:flutter_app/lang/l10n.dart';
+import 'package:flutter_app/ui/component/card/home_item_card.dart';
 import 'package:flutter_app/ui/drawer/drawer_view.dart';
 import 'package:flutter_app/ui/home/home_view_model.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -40,19 +41,28 @@ class HomeView extends HookConsumerWidget {
         title: Text(l10n.home),
       ),
       drawer: const DrawerView(),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            homeViewModel.when(
+      body: Column(
+        children: [
+          Expanded(
+            child: homeViewModel.when(
                 data: (data) {
-                  return Container();
+                  return GridView.count(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 10,
+                    ),
+                    mainAxisSpacing: 5,
+                    crossAxisCount: 3,
+                    children: [
+                      for (var docs in data.docs!) ...[HomeItemcard(docs: docs)]
+                    ],
+                  );
                 },
                 error: (error, stackTrace) => Container(),
                 loading: () =>
                     const Center(child: CircularProgressIndicator())),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
